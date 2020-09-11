a70474cda8b38f9fdf6b2529be470236b7867945
Andrey Simonov <sasmeo@gmail.com>
2020-09-11 20:42:12 +0800

    Добавил возможность сборки под любую операционку

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const MAX_ENEMY = 3;
+const HEIGHT_ELEM = 100;
+
 const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
     gameArea = document.querySelector('.gameArea'),
@@ -10,6 +13,8 @@ car.classList.add('car');
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
 document.addEventListener('keyup', stopRun);
+
+gameArea.style.height = Math.floor(document.documentElement.clientHeight / HEIGHT_ELEM) * HEIGHT_ELEM;
 
 const keys = {
     ArrowUp: false,
@@ -26,29 +31,29 @@ const setting = {
 };
 
 function getQuantityElements(heightElement) {
-    return document.documentElement.clientHeight / heightElement + 1;
+    return (gameArea.offsetHeight / heightElement) + 1;
 }
 
 function startGame() {
     start.classList.add('hide');
     gameArea.innerHTML = '';
 
-    for (let i = 0; i < getQuantityElements(100); i++) {
+    for (let i = 0; i < getQuantityElements(HEIGHT_ELEM); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
-        line.style.top = `${i * 100}px`;
-        line.y = i * 100;
+        line.style.top = `${i * HEIGHT_ELEM}px`;
+        line.y = i * HEIGHT_ELEM;
         gameArea.appendChild(line);
-
     }
 
-    for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+    for (let i = 0; i < getQuantityElements(HEIGHT_ELEM * setting.traffic); i++) {
         const enemy = document.createElement('div');
+        const randomEnemy = Math.floor(Math.random() * MAX_ENEMY) + 1;
         enemy.classList.add('enemy');
-        enemy.y = -100 * setting.traffic * (i + 1);
-        enemy.style.left = `${Math.floor(Math.random() * (gameArea.offsetWidth -50))}px`;
+        enemy.y = -HEIGHT_ELEM * setting.traffic * (i + 1);
+        enemy.style.left = `${Math.floor(Math.random() * (gameArea.offsetWidth - enemy.offsetWidth/2))}px`;
         enemy.style.top = enemy.y + 'px';
-        enemy.style.background = `transparent url(\'image/enemy${Math.floor(Math.random()*3)+1}.png\') center/cover no-repeat`;
+        enemy.style.background = `transparent url(\'image/enemy${randomEnemy}.png\') center/cover no-repeat`;
         gameArea.appendChild(enemy);
     }
     setting.score = 0;
@@ -103,8 +108,8 @@ function moveRoad() {
     lines.forEach(function(line) {
         line.y += setting.speed;
         line.style.top = line.y + 'px';
-        if (line.y > document.documentElement.clientHeight) {
-            line.y = -100;
+        if (line.y > gameArea.offsetHeight) {
+            line.y = -HEIGHT_ELEM;
         }
     });
 }
@@ -123,9 +128,9 @@ function moveEnemy() {
         }
         enemy.y += setting.speed / 2;
         enemy.style.top = enemy.y + 'px';
-        if (enemy.y > document.documentElement.clientHeight) {
-            enemy.y = -100 * setting.traffic;
-            enemy.style.left = `${Math.floor(Math.random() * (gameArea.offsetWidth -50))}px`;
+        if (enemy.y >= gameArea.offsetHeight) {
+            enemy.y = -HEIGHT_ELEM * setting.traffic;
+            enemy.style.left = `${Math.floor(Math.random() * (gameArea.offsetWidth - enemy.offsetWidth/2))}px`;
             enemy.style.background = `transparent url(\'image/enemy${Math.floor(Math.random()*3)+1}.png\') center/cover no-repeat`;
         }
     });
