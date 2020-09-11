f6fe49daf998182fb752a6661cd92ce94d1e4cae
Andrey Simonov <sasmeo@gmail.com>
2020-09-11 18:31:25 +0800

    Сделал 3 домашку с рандомными машинками final

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ function getQuantityElements(heightElement) {
 
 function startGame() {
     start.classList.add('hide');
+    gameArea.innerHTML = '';
+
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -49,9 +51,12 @@ function startGame() {
         enemy.style.background = `transparent url(\'image/enemy${Math.floor(Math.random()*3)+1}.png\') center/cover no-repeat`;
         gameArea.appendChild(enemy);
     }
-
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -60,6 +65,8 @@ function startGame() {
 function playGame() {
 
     if (setting.start) {
+        setting.score += setting.speed;
+        score.textContent = setting.score;
         moveRoad();
         moveEnemy();
         if (keys.ArrowLeft && setting.x > 0) {
@@ -105,6 +112,15 @@ function moveRoad() {
 function moveEnemy() {
     let enemies = document.querySelectorAll('.enemy');
     enemies.forEach(function(enemy) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = enemy.getBoundingClientRect();
+        if (carRect.top <= enemyRect.bottom &&
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+            setting.start = false;
+            start.classList.remove('hide');
+        }
         enemy.y += setting.speed / 2;
         enemy.style.top = enemy.y + 'px';
         if (enemy.y > document.documentElement.clientHeight) {
